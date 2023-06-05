a426c91306887126f2884818e8799142da9914e4
nikoyak <99891966+nikoyak@users.noreply.github.com>
2023-06-05 23:04:59 +0300

    Bump version to 1.0.1
    ## [1.0.1] - 2023-06-05
    
    No Changes
    
    ### Added
    - Nothing

--- tests/Nyk.SimpleArithmeticLib.Tests/AssemblyInfo.fs
+++ tests/Nyk.SimpleArithmeticLib.Tests/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Nyk.SimpleArithmeticLib.Tests")>]
 [<assembly: AssemblyProductAttribute("Nyk.SimpleArithmeticLib")>]
-[<assembly: AssemblyVersionAttribute("1.0.0")>]
+[<assembly: AssemblyVersionAttribute("1.0.1")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseDate","2023-06-05T00:00:00.0000000+03:00")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.1")>]
+[<assembly: AssemblyInformationalVersionAttribute("1.0.1")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","7bbd484df0032577d0f2a6095258f54c03afdb0b")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","b885080c0b996a5a4f093d89b1ccb99cb384987e")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Nyk.SimpleArithmeticLib.Tests"
     let [<Literal>] AssemblyProduct = "Nyk.SimpleArithmeticLib"
-    let [<Literal>] AssemblyVersion = "1.0.0"
+    let [<Literal>] AssemblyVersion = "1.0.1"
     let [<Literal>] AssemblyMetadata_ReleaseDate = "2023-06-05T00:00:00.0000000+03:00"
-    let [<Literal>] AssemblyFileVersion = "1.0.0"
-    let [<Literal>] AssemblyInformationalVersion = "1.0.0"
+    let [<Literal>] AssemblyFileVersion = "1.0.1"
+    let [<Literal>] AssemblyInformationalVersion = "1.0.1"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "7bbd484df0032577d0f2a6095258f54c03afdb0b"
+    let [<Literal>] AssemblyMetadata_GitHash = "b885080c0b996a5a4f093d89b1ccb99cb384987e"
