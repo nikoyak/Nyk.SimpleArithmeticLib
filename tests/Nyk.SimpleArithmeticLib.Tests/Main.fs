534b16bc19cd81909c4f7d3ad4d4e3be20adef1f
nikoyak <99891966+nikoyak@users.noreply.github.com>
2023-06-05 20:45:28 +0300

    Scaffold

---
+++ tests/Nyk.SimpleArithmeticLib.Tests/Main.fs
@@ -0,0 +1,9 @@
+namespace Nyk.SimpleArithmeticLib.Tests
+
+module ExpectoTemplate =
+
+    open Expecto
+
+    [<EntryPoint>]
+    let main argv =
+        Tests.runTestsInAssembly defaultConfig argv
