d052664810c7d0f00bfb5973e420dd5265015ff8
nikoyak <99891966+nikoyak@users.noreply.github.com>
2023-06-05 22:11:36 +0300

    Bump version to 0.4.0
    ## [0.4.0] - 2023-06-05
    
    Fix test
    
    ### Fixed
    - Fix test.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,19 +1,24 @@
 # Changelog
+
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+## [0.4.0] - 2023-06-05
+
 Fix test
 
 ### Fixed
 - Fix test.
 
 ## [0.1.0] - 2017-03-17
+
 First release
 
 ### Added
 - This release already has lots of features
 
+[Unreleased]: https://github.com/nikoyak/Nyk.SimpleArithmeticLib//compare/v0.4.0...HEAD
+[0.4.0]: https://github.com/nikoyak/Nyk.SimpleArithmeticLib//compare/v0.1.0...v0.4.0
 [0.1.0]: https://github.com/nikoyak/Nyk.SimpleArithmeticLib/releases/tag/v0.1.0
--- tests/Nyk.SimpleArithmeticLib.Tests/AssemblyInfo.fs
+++ tests/Nyk.SimpleArithmeticLib.Tests/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Nyk.SimpleArithmeticLib.Tests")>]
 [<assembly: AssemblyProductAttribute("Nyk.SimpleArithmeticLib")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2017-03-17T00:00:00.0000000")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("0.4.0")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2023-06-05T00:00:00.0000000+03:00")>]
+[<assembly: AssemblyFileVersionAttribute("0.4.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.4.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","bb8964b54bee133e9af64d316dc2cfee16df7f72")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","7bbd484df0032577d0f2a6095258f54c03afdb0b")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Nyk.SimpleArithmeticLib.Tests"
     let [<Literal>] AssemblyProduct = "Nyk.SimpleArithmeticLib"
-    let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2017-03-17T00:00:00.0000000"
-    let [<Literal>] AssemblyFileVersion = "0.1.0"
-    let [<Literal>] AssemblyInformationalVersion = "0.1.0"
+    let [<Literal>] AssemblyVersion = "0.4.0"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2023-06-05T00:00:00.0000000+03:00"
+    let [<Literal>] AssemblyFileVersion = "0.4.0"
+    let [<Literal>] AssemblyInformationalVersion = "0.4.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "bb8964b54bee133e9af64d316dc2cfee16df7f72"
+    let [<Literal>] AssemblyMetadata_GitHash = "7bbd484df0032577d0f2a6095258f54c03afdb0b"
